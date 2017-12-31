50e9d520cffacc6587460262af0175071238b8df
nada <nona6066@gmail.com>
2017-12-31 22:35:51 +0200

    first applet

---
+++ Arrows.java
@@ -0,0 +1,99 @@
+import java.applet.*;
+import java.awt.*;
+import java.util.*;
+import java.text.*;
+import java.applet.Applet;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.KeyAdapter;
+
+
+public class Arrows extends Applet implements Runnable {
+      String s = "Hello Java";
+      Thread t;
+      int x = 100;
+      int y =100;
+ 
+
+      public void init(){
+       t = new Thread(this);
+       t.start();
+
+   addKeyListener(new KeyAdapter() {
+   public void keyPressed(KeyEvent e) {
+    int key = e.getKeyCode();
+
+    if (key == KeyEvent.VK_LEFT) {
+        if(x==0){
+               x=0;
+			}else{
+              x--;
+			}
+			repaint();
+		}
+   
+
+    if (key == KeyEvent.VK_RIGHT) {
+       if(x == getWidth())
+			{
+                x = getWidth();
+			}
+			else
+			{
+				x++ ;
+			}
+      
+  }
+    if (key == KeyEvent.VK_UP) {
+       if(y==0)
+               {
+                	y=0;
+               }else{
+                	y-- ;
+               }
+    }
+
+    if (key == KeyEvent.VK_DOWN) {
+         if(y == getHeight())
+            {
+            	y = getHeight();
+            }else{
+            	y++;
+            }
+    }
+   }
+      // public void keyTyped(KeyEvent e) {
+      // }
+
+      // public void keyPressed(KeyEvent e) {
+      // }
+  });
+}
+      public void run(){
+      	while(true){
+      		repaint();
+
+      		try{
+      			Thread.sleep(300);
+      		}
+      		catch(Exception e ){
+      			e.printStackTrace();
+      			System.out.println("Exception");
+      		}
+      	}        
+
+      }
+
+	      public void paint(Graphics g ){
+	        Font f = new Font("Impact" , Font.BOLD , 66);
+	        g.setColor(Color.RED);  
+	        g.drawString(s, x, y);
+	        
+	        }
+
+}
---
+++ README.md
@@ -0,0 +1,17 @@
+Text Animation-java applet Project
+--------------------------
+
+Documentation
+
+###Steps to run
+
+1- Dawonload Text Animation-java Project 
+
+2- to run java class  got to project directory by commandline and run $ javac Arrows.java
+
+3- to display java applet run command $ appletviewer arrows.html	
+
+applet window will display by text animation 
+----------------------------------------------------------
+
+project uses html and java class like (Applet and etc) and functions like init and paint.
